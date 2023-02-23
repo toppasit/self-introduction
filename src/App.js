c9ce6c6bdf0d84abed3a3fae0537858c97e55b6a
pasit <>
2023-02-24 01:36:26 +0700

    Add route to new page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import ProfilePage from './Page/profile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter>
+        <Routes>
+          <Route path='/' element={<ProfilePage/>}/>
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
